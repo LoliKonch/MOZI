05ffbdd64b8ccdd0d641b29adc8583b19c5a1c9f
LoliKonch <ibugydydygig@gmail.com>
2023-03-21 18:02:07 +0300

    refactor

--- Lab3/No2.py
+++ Lab3/No2.py
@@ -1,7 +1,7 @@
 def binary_NOD(num1, num2):
     shift = 0
-    a = d = 1
-    b = c = 0
+    a, d = 1, 1
+    b, c = 0, 0
 
     while (num1 | num2) & 1 == 0:
         shift += 1
@@ -10,40 +10,40 @@ def binary_NOD(num1, num2):
     x = num1
     y = num2
 
-    while num1 != 0:
-        while num1 & 1 == 0:
-            num1 >>= 1
+    while x != 0:
+        while x & 1 == 0:
+            x >>= 1
             if (a | b) & 1 == 0:
                 a >>= 1
                 b >>= 1
             else:
-                a += y
-                b -= x
+                a += num2
+                b -= num1
                 a >>= 1
                 b >>= 1
 
 
-        while num2 & 1 == 0:
-            num2 >>= 1
+        while y & 1 == 0:
+            y >>= 1
             if (c | d) & 1 == 0:
                 c >>= 1
                 d >>= 1
             else:
-                c += y
-                d -= x
+                c += num2
+                d -= num1
                 c >>= 1
                 d >>= 1
 
-        if num1 >= num2:
-            num1 -= num2
+        if x >= y:
+            x -= y
             a -= c
             b -= d
         else:
-            num2 -= num1
+            y -= x
             c -= a
             d -= b
 
-    return num2 << shift, c, d
+    return y << shift, c, d
 
 A = 5949114
 B = 3941396
