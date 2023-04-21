8a4e54d0e8fdf1eae4008524261dfecb8bd13488
LoliKonch <ibugydydygig@gmail.com>
2023-04-21 19:07:49 +0300

    Lab8

---
+++ Lab8/No1.py
@@ -0,0 +1,37 @@
+from Lab7 import Prepare_No12
+import random
+
+
+for n in Prepare_No12.throwMeSomeNumbers():
+    k = [random.randint(2, n - 2) for i in range(5)]
+    number = n - 1
+    s = 0
+    t = 0
+
+    while True:
+        if number % 2 == 0:
+            s += 1
+            number /= 2
+        else:
+            t =  int(number)
+            break
+
+    for i in k:
+        flag = False
+        for r in range(s):
+            x = pow(i, (2 ** r) * t, n)
+
+            if  x == 1 or x == n - 1:
+                flag = True
+                break
+            else:
+                flag = False
+        if not flag:
+            break
+
+
+
+    if flag:
+        print(n, "Прошло тест")
+    else:
+        print(n, "Не прошло тест")
